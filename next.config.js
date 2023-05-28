b953ca0cb419e946b5c7eef51c4353219f4ccc0f
jayw1112 <housexpresso@gmail.com>
2023-05-28 04:43:38 -0700

    next-pwa and added manifest

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+})
+
+module.exports = withPWA({
+  // next.js config
+  ...nextConfig,
+})
